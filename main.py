0bde3338b50fbe3e79621c63fd2a6f27ce69177b
pefilippov <pefilippov@ozon.ru>
2020-04-26 03:55:04 +0300

    test login to instagram

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #main.py
 from selenium import webdriver
 from time import sleep
+from secrets import pw
 
 class InstaBot:
-    def __init__(self):
+    def __init__(self, username, pw):
         print("test")
         self.driver = webdriver.Chrome()
-        self.driver.get("https://instagram.com")
+        self.driver.get("https://www.instagram.com/accounts/login/")
         sleep(5)
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type="submit"]')\
+            .click()
+        sleep(999)
 
-InstaBot()
+InstaBot("ptflp", pw)
