42fb3cc1c721957755cb2e91983d825c5b902742
pefilippov <pefilippov@ozon.ru>
2020-04-26 05:40:58 +0300

    check fit jope rope gop nope :D

--- main.py
+++ main.py
@@ -2,19 +2,32 @@
 from selenium import webdriver
 from time import sleep
 from secrets import pw
+from secrets import login
 
 class InstaBot:
     def __init__(self, username, pw):
         print("test")
+        self.username = username
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.instagram.com/accounts/login/")
-        sleep(5)
+        sleep(1.618)
         self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
             .send_keys(username)
+        sleep(1.618)
         self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
             .send_keys(pw)
+        sleep(1.618)
         self.driver.find_element_by_xpath('//button[@type="submit"]')\
             .click()
-        sleep(999)
+        sleep(3)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+        sleep(2)
+    def getUnfollowers(self):
+        print("starting getUnfollowers")
+        self.driver.find_element_by_xpath("//a[contains(@href,'/{}')]".format(self.username))\
+            .click()
+        sleep(2)
 
-InstaBot("ptflp", pw)
+bot = InstaBot(login, pw)
+bot.getUnfollowers()
